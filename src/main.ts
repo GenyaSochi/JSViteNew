40f2aee97058ca1a5befc5e52c7016b3ed096eb3
GenyaSochi <eunasedkina@gmail.com>
2024-06-11 11:54:38 +0300

    _

--- src/main.ts
+++ src/main.ts
@@ -2885,10 +2885,10 @@ function newDate() {
   let minutes = date.getMinutes()
   let seconds = date.getSeconds()
   if (hours < 10)
-    hours = +'0' + hours
+    hours = + '0' + hours
   clock.children[0].innerHTML = hours + ""
   if (minutes < 10)
-    minutes = +'0' + minutes
+    minutes = + '0' + minutes
   clock.children[1].innerHTML = minutes + ""
   if (seconds < 10)
     seconds = + '0' + seconds
