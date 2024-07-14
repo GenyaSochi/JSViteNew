d777f1ddfe2dbb8d02169d271fa6e24d3df37d4f
GenyaSochi <eunasedkina@gmail.com>
2024-07-14 12:26:45 +0300

    _

--- src/main.ts
+++ src/main.ts
@@ -3386,30 +3386,86 @@ view.addEventListener('click', () => {
 
 //Сделайте ячейки таблицы редактируемыми по клику.
 
-let currentTD = null as HTMLTableCellElement | null
-let currentTDColor = null as string | null
-const baguaTable = document.querySelector('#bagua-table') as HTMLTableElement
-baguaTable.addEventListener('click', (e) => {
-  const target = e.target as HTMLElement
-  const td = target.closest('td')
-  if (!td) return
-  td.style.padding = '0'
-  td.style.backgroundColor = 'white'
-  const data = td.innerHTML
-  td.innerHTML = `<textarea style="width:96%; height:94%">${data}</textarea>`
-  td.classList.add('edit-td')
-  let textArea = document.createElement('textarea')
-  textArea.style.width = td.clientWidth + 'px'
-  textArea.style.height = td.clientHeight + 'px'
-  textArea.className = 'edit-area'
-  textArea.value = td.innerHTML
-  td.innerHTML = ''
-  td.appendChild(textArea)
-  textArea.focus()
+// let currentTD = null as HTMLTableCellElement | null | string
+// let currentTDColor = null as string | null
+// const baguaTable = document.querySelector('#bagua-table') as HTMLTableElement
+// baguaTable.addEventListener('click', (e) => {
+//   const target = e.target as HTMLElement
+//   const td = target.closest('td')
+//   if (!td) return
+//   td.style.padding = '0'
+//   td.style.backgroundColor = 'white'
+//   const data = td.innerHTML
+//   td.innerHTML = `<textarea style="width:96%; height:94%">${data}</textarea>`
+//   td.classList.add('edit-td')//редактирование
+//   let textArea = document.createElement('textarea')
+//   textArea.style.width = td.clientWidth + 'px'
+//   textArea.style.height = td.clientHeight + 'px'
+//   textArea.className = 'edit-area'
+//   textArea.value = td.innerHTML
+//   td.innerHTML = ''
+//   td.appendChild(textArea)
+//   textArea.focus()
+//   e.preventDefault()
 
- 
+//   //insertAdjacentHTML позволяет вставитьстроку HTML кода в любое место страницы.
+//   // @ts-ignore
+//   td.insertAdjacentHTML("beforeEnd",
+//     '<div class="edit-controls"><button class="edit-ok">OK</button><button class="edit-cancel">CANCEL</button></div>'
+//   )  
+// })
+// function applyBtn(td, OK){
+//   if(!OK)
+//     return
+  
+let table = document.getElementById('bagua-table');
+let editingTd;
 
-})
+table.onclick = function(event) {
+  // 3 возможных цели
+  let target = event.target.closest('.edit-cancel,.edit-ok,td');
+  if (!table.contains(target)) return;
+  if (target.className == 'edit-cancel') {
+    finishTdEdit(editingTd.elem, false);
+  } else if (target.className == 'edit-ok') {
+    finishTdEdit(editingTd.elem, true);
+  } else if (target.nodeName == 'TD') {
+    if (editingTd) return; // уже редактируется
 
-//Добавьте пункт к выпадающему списку.
-//resize: none;
+    makeTdEditable(target);
+  }
+
+};
+
+function makeTdEditable(td) {
+  editingTd = {
+    elem: td,
+    data: td.innerHTML
+  };
+
+  td.classList.add('edit-td'); // td в состоянии редактирования, CSS применятся к textarea внутри ячейки
+
+  let textArea = document.createElement('textarea');
+  textArea.style.width = td.clientWidth + 'px';
+  textArea.style.height = td.clientHeight + 'px'; 
+  textArea.className = 'edit-area';
+
+  textArea.value = td.innerHTML;
+  td.innerHTML = '';
+  td.appendChild(textArea);
+  textArea.focus();
+
+  td.insertAdjacentHTML("beforeEnd",
+    '<div class="edit-controls"><button class="edit-ok">OK</button><button class="edit-cancel">CANCEL</button></div>'
+  );
+}
+
+function finishTdEdit(td, isOk) {
+  if (isOk) {
+    td.innerHTML = td.firstChild.value;
+  } else {
+    td.innerHTML = editingTd.data;
+  }
+  td.classList.remove('edit-td');
+  editingTd = null;
+}
